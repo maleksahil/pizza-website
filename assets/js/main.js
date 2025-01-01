ec5f83baa174236b4c05c0948eaba3200f4306ce
sahil <maleksahil9315@gmail.com>
2025-01-02 03:24:10 +0530

    pizza

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,7 +59,39 @@ const scrollup = () =>{
                         : scrollUp.classList.remove('show-scroll')
 }
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+   const sections = document.querySelectorAll('section[id]');
 
+   const scrollActive = () =>{
+      const scrollDown = window.scrollY
+
+      sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop-58,
+              sectionId = current.getAttribute('id'),
+              sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+              if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+                sectionClass.classList.add('active-link')
+              }else{
+                sectionClass.classList.remove('active-link')
+              }
+      })
+   }
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 300,
+    //reset: true, //Animations repeat
+})
+
+sr.reveal(`.home__data .popular__container, .footer`)
+sr.reveal(`.home__board`, {delay: 700, distance: '100px', origin: 'right'})
+sr.reveal(`.home__pizza`, {delay: 1400, distance: '100px', origin: 'bottom', rotate: {z: -90}})
+sr.reveal(`.home__ingredient`, {delay: 2000, interval: 100})
+sr.reveal(`.about__data, .recipe__list, .contact__data`, {origin: 'right'})
+sr.reveal(`.about__img, .recipe__image, .contact__img`, {origin: 'left'})
+sr.reveal(`.products__card`,{interval: 100})
